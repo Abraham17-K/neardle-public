b9279b845b5c5bd84683755763877cc17b0d63af
Media <719coolboy@gmail.com>
2022-04-02 15:49:00 -0400

    Update

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use("/bootstrap", express.static(__dirname + '/node_modules/bootstrap/dist'));
+
 //Express Static
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-     // res.send(wordList.accept[Math.floor(Math.random() * words.length)])
-     res.render(__dirname + '/public/index');
+     res.sendFile(__dirname + '/public/index');
 });
 
 //Express Listen
 app.listen(port, () => {
      console.log(`Neardle running on http://localhost:${port}`)
-})
-
-app.post("/getWord", (req, res) => {
-     
 })
--- public/script.js
+++ public/script.js
@@ -1,9 +1,32 @@
 
-Var word;
+var currentWord = [];
 
-document.addEventListener('keydown', function(e) {
-     console.log(e.key);
-     if (word.length < 5) word += e.key;
-     else word = e.key;
-     document.getElementById('word').innerHTML = word;
+//Listens for keypresses
+document.addEventListener("keydown", (e) => {
+     if(e.key == "Backspace") {
+          currentWord.pop()
+          return
+     }
+     else if(e.key == "Enter") {
+          console.log(toWord(currentWord))
+          document.getElementById("worded").innerText = toWord(currentWord)
+          return
+     }
+     else if(currentWord.length >= 5) return
+     else if(e.keyCode >= 65 && e.keyCode <= 90) {
+          currentWord.push(e.key.toLocaleUpperCase())
+     }
 });
+
+//Converts the array of currentWord to a string
+function toWord() {
+     let word = ""
+     for (var i = 0; i < currentWord.length; i++) {
+          word += currentWord[i];
+     }
+     return word
+}
+
+function checkWord() {
+    accepted.indexOf(toWord(currentWord)) != -1 ? alert("Correct!") : alert("Incorrect!")
+}
