13addcf2eafda9460c8fa200826fc2d15d26e7b9
Media <719coolboy@gmai.com>
2022-04-03 08:24:10 -0400

    Fixed JS, Adding Colors

--- public/script.js
+++ public/script.js
@@ -1,20 +1,28 @@
+var board = document.getElementById("board");
 var currentWord = [];
+var currentRow = 0;
+var rows = board.children
+var currentDistance = [];
+var correct = "#68cf30"
+var colors = []
+// console.log(Math.abs('a'.charCodeAt(0)-'c'.charCodeAt(0)))
 
 //Listens for keypresses
 document.addEventListener("keydown", (e) => {
      if(e.key == "Backspace") {
           currentWord.pop()
-          return
+          updateBoard();
      }
      else if(e.key == "Enter") {
-          console.log(toWord(currentWord))
-          document.getElementById("worded").innerText = toWord(currentWord)
-          return
+          validateBoard()
+          colorBoard()
      }
      else if(currentWord.length >= 5) return
      else if(e.keyCode >= 65 && e.keyCode <= 90) {
-          currentWord.push(e.key.toLocaleUpperCase())
+          currentWord.push(e.key.toLocaleLowerCase())
      }
+     updateBoard()
+     console.log(currentWord)
 });
 
 //Converts the array of currentWord to a string
@@ -27,5 +35,38 @@ function toWord() {
 }
 
 function checkWord() {
-    accepted.indexOf(toWord(currentWord)) != -1 ? alert("Correct!") : alert("Incorrect!")
+     let word = toWord(currentWord)
+     return (accepted.includes(word) || answers.includes(word))
+}
+
+function updateBoard() {
+     let slots = rows[currentRow].children
+     for (var i = 0; i < slots.length; i++) {
+          if (currentWord[i] != undefined) {
+               slots[i].innerText = currentWord[i].toLocaleUpperCase() 
+          } else {
+               slots[i].innerText = ""
+          }
+     }
+}
+
+function colorBoard() {
+     let slots = rows[currentRow].children
+     for(let i = 0; i < currentWord.length; i++) {
+
+     }
+}
+
+function validateBoard() {
+     console.log(currentWord.length)
+     if (currentWord.length == 5 && checkWord()) {
+          currentWord = []
+          currentRow++
+     } else if (currentWord.length < 5) {
+          alert("Word is too short");
+          return;
+     } else {
+          alert("Word is not in the dictionary");
+          return;
+     }
 }
